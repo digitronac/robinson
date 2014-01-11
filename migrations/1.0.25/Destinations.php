73bca76f14fde1cac67083603c7940bb435a704f
Nemanja Ognjanovic <ognjanovic@gmail.com>
2014-01-11 23:13:22 +0100

    added pk to destinationId :)

---
+++ migrations/1.0.25/Destinations.php
@@ -0,0 +1,105 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Mvc\Model\Migration;
+
+class DestinationsMigration_1025 extends Migration
+{
+
+    public function up()
+    {
+        $this->morphTable(
+            'Destinations',
+            array(
+            'columns' => array(
+                new Column(
+                    'destinationId',
+                    array(
+                        'type' => Column::TYPE_INTEGER,
+                        'notNull' => true,
+                        'autoIncrement' => true,
+                        'size' => 10,
+                        'first' => true
+                    )
+                ),
+                new Column(
+                    'destination',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => true,
+                        'size' => 255,
+                        'after' => 'destinationId'
+                    )
+                ),
+                new Column(
+                    'description',
+                    array(
+                        'type' => Column::TYPE_TEXT,
+                        'notNull' => true,
+                        'size' => 1,
+                        'after' => 'destination'
+                    )
+                ),
+                new Column(
+                    'status',
+                    array(
+                        'type' => Column::TYPE_INTEGER,
+                        'notNull' => true,
+                        'size' => 1,
+                        'after' => 'description'
+                    )
+                ),
+                new Column(
+                    'createdAt',
+                    array(
+                        'type' => Column::TYPE_DATETIME,
+                        'notNull' => true,
+                        'size' => 1,
+                        'after' => 'status'
+                    )
+                ),
+                new Column(
+                    'updatedAt',
+                    array(
+                        'type' => Column::TYPE_DATETIME,
+                        'notNull' => true,
+                        'size' => 1,
+                        'after' => 'createdAt'
+                    )
+                ),
+                new Column(
+                    'categoryId',
+                    array(
+                        'type' => Column::TYPE_INTEGER,
+                        'notNull' => true,
+                        'size' => 11,
+                        'after' => 'updatedAt'
+                    )
+                )
+            ),
+            'indexes' => array(
+                new Index('PRIMARY', array('destinationId')),
+                new Index('destinationId', array('destinationId')),
+                new Index('DestinationsStatus_INDEX', array('status')),
+                new Index('DestinationsCategoryId_FK', array('categoryId'))
+            ),
+            'references' => array(
+                new Reference('DestinationsCategoryId_FK', array(
+                    'referencedSchema' => 'robinson_development',
+                    'referencedTable' => 'Category',
+                    'columns' => array('categoryId'),
+                    'referencedColumns' => array('categoryId')
+                ))
+            ),
+            'options' => array(
+                'TABLE_TYPE' => 'BASE TABLE',
+                'AUTO_INCREMENT' => '10',
+                'ENGINE' => 'InnoDB',
+                'TABLE_COLLATION' => 'utf8_unicode_ci'
+            )
+        )
+        );
+    }
+}
---
+++ migrations/1.0.25/ImageCategory.php
@@ -0,0 +1,93 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Mvc\Model\Migration;
+
+class ImagecategoryMigration_1025 extends Migration
+{
+
+    public function up()
+    {
+        $this->morphTable(
+            'ImageCategory',
+            array(
+            'columns' => array(
+                new Column(
+                    'imageCategoryId',
+                    array(
+                        'type' => Column::TYPE_INTEGER,
+                        'notNull' => true,
+                        'autoIncrement' => true,
+                        'size' => 11,
+                        'first' => true
+                    )
+                ),
+                new Column(
+                    'filename',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => true,
+                        'size' => 255,
+                        'after' => 'imageCategoryId'
+                    )
+                ),
+                new Column(
+                    'createdAt',
+                    array(
+                        'type' => Column::TYPE_DATETIME,
+                        'notNull' => true,
+                        'size' => 1,
+                        'after' => 'filename'
+                    )
+                ),
+                new Column(
+                    'categoryId',
+                    array(
+                        'type' => Column::TYPE_INTEGER,
+                        'notNull' => true,
+                        'size' => 11,
+                        'after' => 'createdAt'
+                    )
+                ),
+                new Column(
+                    'sort',
+                    array(
+                        'type' => Column::TYPE_INTEGER,
+                        'notNull' => true,
+                        'size' => 11,
+                        'after' => 'categoryId'
+                    )
+                ),
+                new Column(
+                    'extension',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'size' => 11,
+                        'after' => 'sort'
+                    )
+                )
+            ),
+            'indexes' => array(
+                new Index('PRIMARY', array('imageCategoryId')),
+                new Index('IDX_AD1C86C39C370B71', array('categoryId'))
+            ),
+            'references' => array(
+                new Reference('FK_AD1C86C39C370B71', array(
+                    'referencedSchema' => 'robinson_development',
+                    'referencedTable' => 'Category',
+                    'columns' => array('categoryId'),
+                    'referencedColumns' => array('categoryId')
+                ))
+            ),
+            'options' => array(
+                'TABLE_TYPE' => 'BASE TABLE',
+                'AUTO_INCREMENT' => '10',
+                'ENGINE' => 'InnoDB',
+                'TABLE_COLLATION' => 'utf8_unicode_ci'
+            )
+        )
+        );
+    }
+}
