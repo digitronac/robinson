75cf3b6a2dfb785dea906d8f3ecb9bfa0842d2a5
Nemanja Ognjanovic <ognjanovic@gmail.com>
2014-01-16 08:14:31 +0100

    added create method to package

--- apps/backend/models/Package.php
+++ apps/backend/models/Package.php
@@ -2,4 +2,79 @@
 namespace Robinson\Backend\Models;
 class Package extends \Phalcon\Mvc\Model
 {
+    const STATUS_INVISIBLE = 0;
+    const STATUS_VISIBLE = 1;
+    
+    protected static $statusMessages = array
+    (
+        self::STATUS_INVISIBLE => 'nevidljiv',
+        self::STATUS_VISIBLE => 'vidljiv',
+    );
+    
+    protected $packageId;
+    
+    protected $package;
+    
+    protected $description;
+    
+    protected $tabs;
+    
+    protected $price;
+    
+    protected $pdf;
+    
+    protected $status;
+    
+    protected $createdAt;
+    
+    protected $updatedAt;
+    
+    /**
+     *
+     * @var \Phalcon\Http\Request\File  
+     */
+    protected $uploadedPdf;
+    
+    /**
+     * Called when new package is created.
+     * 
+     * @return void
+     */
+    public function create()
+    {
+        if (is_null($this->createdAt))
+        {
+            $this->createdAt = (new \DateTime('now', date_default_timezone_get()))->format('Y-m-d H:i:s');
+        }
+        
+        if (is_null($this->updatedAt))
+        {
+            $this->updatedAt = (new \DateTime('now', date_default_timezone_get()))->format('Y-m-d H:i:s');
+        }
+        
+        return $this->parentCreate();
+    }
+    
+    /**
+     * Event which is trigger after calling self::parentCreate.
+     * 
+     * @return void
+     */
+    public function afterCreate()
+    {
+        
+    }
+    
+    /**
+     * Overriden create method.
+     * 
+     * @param array $data      data
+     * @param array $whitelist data
+     * 
+     * @return bool
+     */
+    public function parentCreate($data = null, $whitelist = null)
+    {
+        return parent::create($data, $whiteList);
+    }
 }
