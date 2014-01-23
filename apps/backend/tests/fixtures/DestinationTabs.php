5f738e5e0939bb762c5c1d89fa5cd7dbd20262e3
Nemanja Ognjanovic <ognjanovic@gmail.com>
2014-01-23 22:13:09 +0100

    added fixture , refs #16

---
+++ apps/backend/tests/fixtures/DestinationTabs.php
@@ -0,0 +1,24 @@
+<?php
+namespace Phalcon\Test\Fixtures;
+// @codingStandardsIgnoreStart
+class DestinationTabs
+{
+    public static function get($records = null)
+    {
+        // destinationTabId, title, description, type, destinationId, createdAt, updatedAt
+        $template = "(%d, '%s', '%s', %d, '%s', %d, '%s', '%s', %d)";
+        
+        for ($i = 1; $i <= 5; $i++)
+        {
+            $type = 2;
+            if($i %2)
+            {
+                $type = 1;
+            }
+            $data[] = sprintf($template, $i, "title-{$i}", "description-{$i}", $type, 1, "2014-01-17 1{$i}:00:00", "2014-01-17 1{$i}:00:00'");
+            //$data[] = "($i, 'package{$i}', 'description{$i}', 999, 'pdffile-1.pdf', 0, '2014-01-17 1{$i}:00:00', '2014-01-17 1{$i}:00:00', 1)";
+        }
+        
+        return $data;
+    }
+}
