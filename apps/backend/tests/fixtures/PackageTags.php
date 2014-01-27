5c2c62b1af53a6203ab39fe0d7311fceefb87ec1
Nemanja Ognjanovic <ognjanovic@gmail.com>
2014-01-27 14:24:12 +0100

    started testing tags, refs #20

---
+++ apps/backend/tests/fixtures/PackageTags.php
@@ -0,0 +1,24 @@
+<?php
+namespace Phalcon\Test\Fixtures;
+// @codingStandardsIgnoreStart
+class PackageTags
+{
+    public static function get($records = null)
+    {
+        // packageTagId, tag, type, packageId, createdAt
+        $template = "(%d, '%s', %d, %d, '%s')";
+        
+        for ($i = 1; $i <= 3; $i++)
+        {
+            $j = 0;
+            foreach(\Phalcon\DI::getDefault()->getShared('config')->application->package->tags as $type => $tag)
+            {
+                $i = $i+$j;
+                $data[] = sprintf($template, $i, "tag title-{$i}", 1, 3, "2014-01-17 1{$i}:00:00");
+                $j++;
+            }
+        }
+        
+        return $data;
+    }
+}
---
+++ apps/backend/tests/models/Tags/PackageTagsTest.php
@@ -0,0 +1,18 @@
+<?php
+// @codingStandardsIgnoreStart
+namespace Robinson\Backend\Tests\Models\Tags;
+class PackageTagTest extends \Robinson\Backend\Tests\Models\BaseTestModel
+{
+    protected function setUp(\Phalcon\DiInterface $di = null, \Phalcon\Config $config = null)
+    {
+        parent::setUp($di, $config);
+        $this->populateTable('packages');
+        $this->populateTable('package_tags');
+    }
+    
+    public function testCanMakeModel()
+    {
+        $model = $this->getDI()->get('Robinson\Backend\Models\Tags\Package');
+        $this->assertInstanceOf('Robinson\Backend\Models\Tags\Package', $model);
+    }
+}
