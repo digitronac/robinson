4f76e18307544b1d1cb13c3f1cfb69ebb11f875f
Nemanja Ognjanovic <ognjanovic@gmail.com>
2014-01-27 22:57:22 +0100

    format

--- apps/backend/Module.php
+++ apps/backend/Module.php
@@ -3,67 +3,72 @@
 namespace Robinson\Backend;
 
 use Phalcon\Loader,
-Phalcon\Mvc\View,
+    Phalcon\Mvc\View,
     Phalcon\Db\Adapter\Pdo\Mysql as DbAdapter,
     Phalcon\Mvc\ModuleDefinitionInterface;
 
 class Module implements ModuleDefinitionInterface
 {
 
-        /**
-         * Registers the module auto-loader.
-         * 
-         * @return void
-         */
-        public function registerAutoloaders()
+    /**
+     * Registers the module auto-loader.
+     *
+     * @return void
+     */
+    public function registerAutoloaders()
+    {
+        if (!defined('MODULE_PATH'))
         {
-            if (!defined('MODULE_PATH'))
-            {
-                define('MODULE_PATH', __DIR__);
-            }
+            define('MODULE_PATH', __DIR__);
+        }
 
-            $loader = new Loader();
+        $loader = new Loader();
 
-            $loader->registerNamespaces(array
+        $loader->registerNamespaces(
+            array
             (
                 'Robinson\Backend\Controllers' => __DIR__ . '/controllers/',
-                'Robinson\Backend\Models' => __DIR__ . '/models/',
-                'Robinson\Backend\Plugin' => __DIR__ . '/plugins/',
-                'Robinson\Backend\Validator' => __DIR__ . '/validators/',
-                'Robinson\Backend\Tag' => __DIR__ . '/tags/',
-                'Robinson\Backend\Filter' => __DIR__ . '/filters/',
-            ));
+                'Robinson\Backend\Models'      => __DIR__ . '/models/',
+                'Robinson\Backend\Plugin'      => __DIR__ . '/plugins/',
+                'Robinson\Backend\Validator'   => __DIR__ . '/validators/',
+                'Robinson\Backend\Tag'         => __DIR__ . '/tags/',
+                'Robinson\Backend\Filter'      => __DIR__ . '/filters/',
+            )
+        );
 
-            $loader->register();
-        }
+        $loader->register();
+    }
 
-        /**
-         * Registers the module-only services
-         *
-         * @param Phalcon\DI $di di
-         * 
-         * @return \Phalcon\DI
-         */
-        public function registerServices($di)
+    /**
+     * Registers the module-only services
+     *
+     * @param Phalcon\DI $di di
+     *
+     * @return \Phalcon\DI
+     */
+    public function registerServices($di)
+    {
+        $config = new \Phalcon\Config\Adapter\Ini(MODULE_PATH . '/config/application.ini');
+        if (is_file(MODULE_PATH . '/config/application.local.ini'))
         {
-                $config = new \Phalcon\Config\Adapter\Ini(MODULE_PATH . '/config/application.ini');
-                if (is_file(MODULE_PATH . '/config/application.local.ini'))
-                {
-                    $local = (new \Phalcon\Config\Adapter\Ini(__DIR__ . '/config/application.local.ini'));
-                    $config->merge($local);
-                }
-                $config = $config->get(APPLICATION_ENV);
-
-                include APPLICATION_PATH . '/backend/config/services.php';
-
-                // listen for exceptions if debug ip
-                if (in_array($di->getService('request')->resolve()->getClientAddress(), 
-        $di->getService('config')->resolve()->application->debug->ips->toArray()))
-                {
-                    (new \Phalcon\Debug())->listen();
-                }
-
-                return $di;
-
+            $local = (new \Phalcon\Config\Adapter\Ini(__DIR__ . '/config/application.local.ini'));
+            $config->merge($local);
         }
+        $config = $config->get(APPLICATION_ENV);
+
+        include APPLICATION_PATH . '/backend/config/services.php';
+
+        // listen for exceptions if debug ip
+        if (in_array(
+            $di->getService('request')->resolve()->getClientAddress(),
+            $di->getService('config')->resolve()->application->debug->ips->toArray()
+        )
+        )
+        {
+            (new \Phalcon\Debug())->listen();
+        }
+
+        return $di;
+
+    }
 }
