f2697fb812e55e35852c2a6f08d0a8870ddaa078
Nemanja Ognjanovic <ognjanovic@gmail.com>
2014-02-09 23:37:36 +0100

    working on destination single page

--- apps/frontend/models/Package.php
+++ apps/frontend/models/Package.php
@@ -238,6 +238,45 @@ class Package extends \Phalcon\Mvc\Model
         return self::$types;
     }
 
+    public function getShortDescription()
+    {
+        return self::truncateText($this->description, 250);
+    }
+
+
+    /**
+     * Truncates text to given char limit.
+     *
+     * @param string $text  text which should be truncated
+     * @param int    $limit char limit
+     * @param string $break on which character should truncate happen
+     * @param string $pad   padding string, will be appended on end of truncated string
+     *
+     * @return string truncated text
+     */
+    public static function truncateText($text, $limit, $break = ".", $pad = "...")
+    {
+        // Original PHP code by Chirp Internet: www.chirp.com.au
+        // Please acknowledge use of this code by including this header
+
+        $text = strip_tags($text);
+        // return with no change if string is shorter than $limit
+        if (mb_strlen($text) <= $limit) {
+            return $text;
+        }
+        // is $break present between $limit and the end of the string?
+        if (false !== ($breakpoint = mb_strpos($text, $break, $limit))) {
+            if ($breakpoint < mb_strlen($text) - 1) {
+                $text = mb_substr($text, 0, $breakpoint) . $pad;
+
+            }
+
+        }
+
+        return trim($text);
+
+    }
+
     /**
      * Finds last minute packages.
      *
