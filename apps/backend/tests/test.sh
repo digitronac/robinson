824bf5ef8b069f125ba81661e671099de7a1ba43
Nemanja Ognjanovic <ognjanovic@gmail.com>
2014-02-19 16:06:04 +0100

    added coverage-checker :)

--- apps/backend/tests/test.sh
+++ apps/backend/tests/test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-php ../../../vendor/bin/phpunit --colors
+php ../../../vendor/bin/phpunit --colors --coverage-clover clover.xml
 php ../../../vendor/bin/phpcs --standard=RAS --ignore="../tests" ../*
+php coverage-checker.php clover.xml 90
