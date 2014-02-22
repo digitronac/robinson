f5963ee42a83d6b28faf0ec74647980ad86c5805
Nemanja Ognjanovic <ognjanovic@gmail.com>
2014-02-22 17:45:44 +0100

    changed watermark again :)

--- apps/backend/filters/Watermark.php
+++ apps/backend/filters/Watermark.php
@@ -29,7 +29,7 @@ class Watermark implements \Phalcon\Filter\UserFilterInterface
         /* @var $imagickFile \Imagick */
         $imagickFile = $args['imagickFile'];
         $destination = $args['destinationFile'];
-        $this->watermark->setimageopacity(0.1);
+        //$this->watermark->setimageopacity(0.1);
         $this->watermark->scaleimage($imagickFile->getimagewidth() / 2, $imagickFile->getimageheight() / 2);
         //$centerwidth = (abs($imagickFile->getimagewidth() - $this->watermark->getimagewidth())) / 2;
         //$centerheight = (abs($imagickFile->getimageheight() - $this->watermark->getimageheight())) / 2;
