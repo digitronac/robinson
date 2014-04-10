0bfb5b511308b6979e58b317ab611fd5ba959026
Nemanja Ognjanovic <ognjanovic@gmail.com>
2014-04-10 20:56:21 +0200

    updated composer and switched to PSR2 standard for backend module

--- apps/backend/tests/test.sh
+++ apps/backend/tests/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 php ../../../vendor/bin/phpunit --colors --coverage-clover clover.xml
-php ../../../vendor/bin/phpcs --standard=RAS --ignore="../tests" ../*
+php ../../../vendor/bin/phpcs --standard=PSR2 --ignore="../tests" ../*
 php coverage-checker.php clover.xml 90
