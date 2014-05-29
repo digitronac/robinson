595fb0e1a456230a7c30265ca278a04677141729
Nemanja Ognjanovic <ognjanovic@gmail.com>
2014-05-29 21:17:37 +0200

    hardcoded categories for easier manual control

--- apps/frontend/controllers/ControllerBase.php
+++ apps/frontend/controllers/ControllerBase.php
@@ -17,13 +17,14 @@ class ControllerBase extends \Phalcon\Mvc\Controller
                 'order' => 'price ASC',
             )
         );
-        $this->view->categories = \Robinson\Frontend\Model\Category::find(
+        /*$this->view->categories = \Robinson\Frontend\Model\Category::find(
             array(
                 'status = ' . \Robinson\Frontend\Model\Category::STATUS_VISIBLE,
                 'order' => 'categoryId ASC',
                 'limit' => 11,
             )
-        );
+        );*/
+        $this->view->categories = $this->getCategories();
 
         $this->view->randomPackages = \Robinson\Frontend\Model\Package::find(
             array(
@@ -33,5 +34,51 @@ class ControllerBase extends \Phalcon\Mvc\Controller
             )
         );
         $this->tag->setTitle('robinson.rs');
+    }
+
+    protected function getCategories()
+    {
+        return array(
+            array(
+                'title' => 'Grčka leto 2014',
+                'categoryId' => 1,
+                'uri' => '/grcka-leto-2014/1'
+            ),
+            array(
+                'title' => 'Španija leto 2014',
+                'categoryId' => 2,
+                'uri' => '/spanija-leto-2014/1',
+            ),
+            array(
+                'title' => 'Italija leto 2014',
+                'categoryId' => 3,
+                'uri' => '/italija-leto-2014',
+            ),
+            array(
+                'title' => 'City break',
+                'categoryId' => 4,
+                'uri' => '/city-break/4',
+            ),
+            array(
+                'title' => 'Formula 1',
+                'categoryId' => 7,
+                'uri' => '/formula-1/7',
+            ),
+            array(
+                'title' => 'Za agente',
+                'categoryId' => 8,
+                'uri' => '/za-agente/8',
+            ),
+            array(
+                'title' => 'Kontakt',
+                'categoryId' => 9,
+                'uri' => '/kontakt/9',
+            ),
+            array(
+                'title' => 'Leto plus',
+                'categoryId' => 10,
+                'uri' => '/leto-plus/10',
+            ),
+        );
     }
 }
