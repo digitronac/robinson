78e3f2ca236810e5ff20acb2a0f19d970faba0fe
Nemanja Ognjanovic <ognjanovic@gmail.com>
2014-07-21 18:19:14 +0200

    removed unused methods , refs #26

--- apps/frontend/models/Tags/Tag.php
+++ apps/frontend/models/Tags/Tag.php
@@ -18,31 +18,10 @@ abstract class Tag extends \Phalcon\Mvc\Model
     
 
     /**
-     * Gets tag title.
-     * 
-     * @return string
-     */
-    public function getTag()
-    {
-        return $this->tag;
-    }
-
-
-    /**
-     * Gets tag type.
-     *
-     * @return int
-     */
-    public function getType()
-    {
-        return (int) $this->type;
-    }
-    
-    /**
      * Resolves tag type to tag title from configuration.
-     * 
+     *
      * @param int $type tag type
-     * 
+     *
      * @return string tag title
      */
     abstract public function resolveTypeToTagTitle($type);
