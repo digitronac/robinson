9f0f4701cf477425b20162242cdc2e8d6869a7f7
Nemanja Ognjanovic <nemanja@ognjanovic.me>
2016-07-08 16:20:18 +0200

    added "decorated" class on menu li

--- apps/frontend/controllers/ControllerBase.php
+++ apps/frontend/controllers/ControllerBase.php
@@ -50,42 +50,50 @@ class ControllerBase extends \Phalcon\Mvc\Controller
             array(
                 'title' => 'Grčka leto 2016',
                 'categoryId' => 1,
-                'uri' => '/grcka-leto-2016/1'
+                'uri' => '/grcka-leto-2016/1',
+                'decorated' => false,
             ),
             array(
                 'title' => 'Španija leto 2016',
                 'categoryId' => 2,
                 'uri' => '/spanija-leto-2016/2',
+                'decorated' => false,
             ),
             array(
                 'title' => 'Italija leto 2016',
                 'categoryId' => 3,
                 'uri' => '/italija-leto-2016/3',
+                'decorated' => false,
             ),
             array(
                 'title' => 'Family club',
                 'categoryId' => 8,
                 'uri' => '/family-club/8',
+                'decorated' => false,
             ),
             array(
                 'title' => 'Skrivena Srbija',
                 'categoryId' => 11,
                 'uri' => '/skrivena-srbija/11',
+                'decorated' => false,
             ),
             array(
                 'title' => 'City break',
                 'categoryId' => 4,
                 'uri' => '/city-break/4',
+                'decorated' => false,
             ),
             array(
                 'title' => 'Formula 1',
                 'categoryId' => 7,
                 'uri' => '/formula-1/7',
+                'decorated' => false,
             ),
             array(
                 'title' => 'Last minute',
                 'categoryId' => null,
                 'uri' => '/index/lastMinute',
+                'decorated' => true,
             )
         );
     }
